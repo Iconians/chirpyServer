ab8e08e0c3a7f9597d8a409f2badce8de6495302
Iconians <claytoncripe@gmail.com>
2025-09-02 12:21:08 -0700

    got a database going and did some schema stuff

--- dist/config.js
+++ dist/config.js
@@ -1,3 +1,11 @@
+process.loadEnvFile();
+export function envOrThrow(key) {
+    const val = process.env[key];
+    if (!val)
+        throw new Error(`Missing environment variable ${key}`);
+    return val;
+}
 export const config = {
     fileserverHits: 0,
+    dbURL: envOrThrow("DB_URL"),
 };
--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,16 @@
+process.loadEnvFile();
 export type APIConfig = {
   fileserverHits: number;
+  dbURL: string;
 };
+
+export function envOrThrow(key: string): string {
+  const val = process.env[key];
+  if (!val) throw new Error(`Missing environment variable ${key}`);
+  return val;
+}
 
 export const config: APIConfig = {
   fileserverHits: 0,
+  dbURL: envOrThrow("DB_URL"),
 };
